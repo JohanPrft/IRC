490c0f63ef1a99e71666a9b9cd277da185207099
42MacAntoine <amontalb@student.42lyon.fr>
2023-09-19 13:30:52 +0200

    bad argument gestion

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,9 +1,20 @@
 #include "../includes/server.hpp"
 
+int	bad_argument(int argc, char **argv)
+{
+	if (argc != 3)
+		return 1;
+	
+	int	port = atoi(argv[1]);
+	if (port == 0)
+		return 1;
+	return (0);
+}
+
 int main(int argc, char **argv)
 {
-	// if (bad_argument(argc, argv))
-	// 	return 1;
+	if (bad_argument(argc, argv))
+		return 1;
 	(void) argc;
 	Server server(atoi(argv[1]), argv[2]);
 	server.initServer();
