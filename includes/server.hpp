927401753a42489b0e825d321aed4d605a5a29eb
42MacAntoine <amontalb@student.42lyon.fr>
2023-09-19 11:46:06 +0200

    class server

---
+++ includes/server.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::cerr;
+
+class Server {
+
+		public :
+
+
+		private :
+			
+			int		_port;
+			int		_serverSocket;
+			string	_password;
+
+
+
+}
