5ddcf438dc7b20324fa9e249e0034101fc78e6fe
42MacAntoine <amontalb@student.42lyon.fr>
2023-09-19 13:00:59 +0200

    gestion listen et fcntl

--- includes/server.hpp
+++ includes/server.hpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <fcntl.h>
 
 using std::cout;
 using std::endl;
@@ -22,7 +23,7 @@ class Server {
 			int			_port;
 			int			_serverSocket;
 			string		_password;
-			sockaddr_in	serverAddress;
+			sockaddr_in	_serverAddress;
 
 
 
--- srcs/server.cpp
+++ srcs/server.cpp
@@ -15,4 +15,29 @@ void	Server::initServer()
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_port = htons(_port);
 	serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+
+	// permet de retutiliser rapidement une socket meme si elle a ete kill
+	const int	enable = 1;
+	setsockopt(_serverSocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+
+	if (bind(_serverSocket, reinterpret_cast<sockaddr*>(&_serverAddress), sizeof(_serverAddress)) == -1)
+	{
+		std::cerr << "Error binding socket" << std::endl;
+		return; // gere ici exeception
+	}
+
+	if (listen(_serverSocket, 5) == -1) //backlog max in queue
+	{
+		std::cerr << "Error listening on socket" << std::endl;
+		return; // gere ici exeception
+	}
+	if (fcntl(_serverSocket, F_SETFL, O_NONBLOCK) == -1)
+	{
+		std::cerr << "Error putting socket on non bloking mode" << std::endl;
+		return; // gere ici exeception
+	}
+	std::cout << "Listening..." << std::endl;
+
+
 }
