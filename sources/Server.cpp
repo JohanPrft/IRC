c47a32ae706502d1a3682119bcd95f525e8dae66
jprofit <jojoman69100@gmail.com>
2023-10-09 14:39:29 +0200

    fix: connecting

--- sources/Server.cpp
+++ sources/Server.cpp
@@ -5,13 +5,13 @@ Server::Server(int port, string password) : _port(port),
                                             _serverSocket(socket(AF_INET, SOCK_STREAM, 0)),
                                             _password(password)
 {
-	if (_serverSocket == -1)
-	{
-		std::cerr << "Error creating socket" << std::endl;
-		return;
-	}
-	cout << "Server password is : " << _password << endl;
-	cout << "Server running on port : " << _port << endl;
+    if (_serverSocket == -1)
+    {
+        std::cerr << "Error creating socket" << std::endl;
+        return;
+    }
+    cout << "Server password is :" << _password << endl;
+    cout << "Server running on port :" << _port << endl;
 }
 
 Server::~Server()
@@ -87,10 +87,10 @@ void Server::handleExistingClient(std::vector<int> &clients, size_t index)
         return;
     }
     try
-	{
-		sendMessage(currentClient, clients);
-	}
-	catch (const std::runtime_error &e)
+    {
+        sendMessage(currentClient, clients);
+    }
+    catch (const std::runtime_error &e)
     {
         handleClientDisconnect(clients, index);
     }
@@ -162,97 +162,11 @@ void Server::handleEvents(std::vector<int> &clients)
     }
 }
 
-	if (listen(_serverSocket, 5) == -1) // backlog max in queue
-	{
-		std::cerr << "Error listening on socket" << std::endl;
-		return; // gere ici exeception
-	}
-	// if (fcntl(_serverSocket, F_SETFL, O_NONBLOCK) == -1)
-	// {
-	// 	std::cerr << "Error putting socket on non bloking mode" << std::endl;
-	// 	return; // gere ici exeception
-	// }
-	std::cout << "Listening..." << std::endl;
+void Server::initServer()
+{
+    initializeServerSocket();
+    addServerSocketToEvents();
 
-	// accept a call
-
-	// Create a pollfd struct for the server socket
-	pollfd serverFd;
-	serverFd.fd = _serverSocket;
-	serverFd.events = POLLIN;
-	_fds.push_back(serverFd);
-
-	std::vector<int> clients;
-	while (true)
-	{
-		// Add the server socket to the pollfd vector
-		// _fds.clear();
-
-		// Add the client sockets to the pollfd vector
-		for (size_t i = 0; i < clients.size(); ++i)
-		{
-			pollfd clientFd;
-			clientFd.fd = clients[i];
-			clientFd.events = POLLIN;
-			_fds.push_back(clientFd);
-		}
-
-		// Use poll to wait for activity on any of the file descriptors
-		int activity = poll(&_fds[0], _fds.size(), -1);
-		if (activity >= 0)
-		{
-			for (size_t i = 0; i < _fds.size(); ++i)
-			{
-				if (_fds[i].revents & POLLIN)
-				{
-					if (_fds[i].fd == _serverSocket)
-					{
-						// New client connection
-						int clientSocket = accept(_serverSocket, NULL, NULL);
-						if (clientSocket == -1)
-						{
-							std::cerr << "Error accepting client socket" << std::endl;
-							return;
-						}
-
-						clients.push_back(clientSocket);
-						_users.insert(std::pair<int, User *>(clientSocket, new User(clientSocket, _password)));
-						map<int, User *>::iterator it = _users.find(clientSocket);
-						std::cout << "New connection from : " << it->second->getFullname() << std::endl;
-					}
-					else
-					{
-						// Existing client has incoming data
-						User *currentClient = _users[_fds[i].fd];
-						if (handleClient(currentClient, clients) == -1)
-						{
-							put_str_fd("Server is disconnecting you now.\n", _fds[i].fd);
-							std::cout << currentClient->getNickname() << " disconnected" << std::endl;
-
-							close(_fds[i].fd);
-							clients.erase(std::remove(clients.begin(), clients.end(), _fds[i].fd), clients.end());
-							delete currentClient;
-							_users.erase(_fds[i].fd);
-							_fds.erase(_fds.begin() + i);
-						}
-					}
-				}
-				else if (_fds[i].revents & POLLHUP)
-				{
-					put_str_fd("Server is disconnecting you now.\n", _fds[i].fd);
-					std::cout << _users[_fds[i].fd]->getNickname() << " disconnected" << std::endl;
-					close(_fds[i].fd);
-					clients.erase(std::remove(clients.begin(), clients.end(), _fds[i].fd), clients.end());
-					delete _users[_fds[i].fd];
-					_users.erase(_fds[i].fd);
-					_fds.erase(_fds.begin() + i);
-				}
-			}
-		}
-		else if (activity == -1)
-		{
-			std::cerr << "Error polling" << std::endl;
-			// return;
-		}
-	}
+    std::vector<int> clients;
+    handleEvents(clients);
 }
